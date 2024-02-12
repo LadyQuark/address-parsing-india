eda9cf565ba749be1064be257b1a17df8d492f2d
Dipika Bothra <dips@Dipikas-iMac.local>
2024-02-12 10:37:01 +0530

    Added tag STATE. Added comments

--- process_excel.py
+++ process_excel.py
@@ -6,20 +6,25 @@ import time
 
 from dotenv import load_dotenv, find_dotenv
 from pathlib import Path
+from spacy import displacy
 from sys import exit
 
-from training_data_prep import massage_data
+from training_data_prep import clean_data
 
 # Get ENV variables
 load_dotenv(find_dotenv())
 MODELS = os.getenv('MODELS', "models/model-best")
 
+# Load model
 nlp = spacy.load(MODELS)
+
+# Constants
 TAGS = [
     "ADDRESS_LINE_1",
     "ADDRESS_LINE_2",
     "CITY",
     "DISTRICT",
+    "STATE",
     "PINCODE",
 ]
 COLUMNS = [
@@ -28,23 +33,23 @@ COLUMNS = [
     "ADDRESS_LINE_2",
     "CITY",
     "DISTRICT",
+    "STATE",
     "PINCODE",
 ]
 
 
-def extract_address(address):
-    cleansed_address = massage_data(address)
+def extract_address_components(address):
+    """
+    Extract components of address using Named Entity Recognition + Rule-based Matching
+    """
+    cleansed_address = clean_data(address)
     doc = nlp(cleansed_address)
-    ent_list = [(ent.text, ent.label_) for ent in doc.ents]
-    # print("Address string -> ", address)
-    # print("Parsed address -> ", str(ent_list))
-    # print("******")
+    displacy.render(doc, style="ent")
 
     results = {tag: "" for tag in TAGS}
     results["ADDRESS"] = cleansed_address
     for ent in doc.ents:
         results[ent.label_] += ent.text if results[ent.label_] == "" else " " + ent.text
-    # print(results)
     return results
 
 def main():
@@ -52,19 +57,23 @@ def main():
     parser.add_argument("-i", "--input", required=True)
     args = parser.parse_args()
 
+    # Read Excel into Pandas Data Frame
     try:
         og_filepath = Path(args.input)
         original = pd.read_excel(args.input, usecols=[0],  dtype=str)
     except FileNotFoundError as e:
         exit(e)
 
+    # Predict
     results = []
     for address in original['Address']:
-        row = extract_address(address)
+        row = extract_address_components(address)
         results.append(row)
     
+    # Write to Excel
     df = pd.DataFrame.from_dict(results)
     df = df.reindex(columns=COLUMNS)
+    
     timestamp = str(int(time.time()))
     output_filepath = og_filepath.with_name(og_filepath.stem + " " + timestamp).with_suffix(".xlsx")
     df.to_excel(output_filepath, index=False)
